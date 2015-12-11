a5c449a588f6136ddf38dfa28e4a26b5d067818b
Moose <moose.techguy@gmail.com>
2015-12-11 01:36:25 -0800

    (feat) Render event titles

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import Data from 'json!./results/stores/eventbrite.json'
 
 var EventBriteEvents = React.createClass({
   getInitialState: function() {
-    return Data;
+    return {
+      events: Data.events
+    }
   }, 
 
   render: function() {
-    console.log(Data);
+    // console.log(Data);
+    console.log(this.state);
     return (
-      <div>{this.state.data}</div>
+      <div>
+      {this.state.events.map(function(event){
+        return <h2>{event.name.text}</h2>
+      })}
+      </div>
 
     )
   }
